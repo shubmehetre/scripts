5c1f9fa389b977440c9b54bbfc875124c83da05d
shubmehetre <ghostfr3k@protonmail.com>
2021-11-29 12:58:50 +0530

    git rm test

--- short_scripts/restore_last_terminal_path.sh
+++
@@ -1,8 +0,0 @@
-# Restore last saved path
-
-last_dir_file=$XDG_CACHE_HOME/last_dir
-touch -a $last_dir_file || touch $last_dir_file
-
-if [ -f $last_dir_file ]
-    then cd `cat $last_dir_file`
-fi
